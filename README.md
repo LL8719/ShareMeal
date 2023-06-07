907cbc517e714fff198adeb5c858f24788015dcf
JosieSavill <120339032+JosieSavill@users.noreply.github.com>
2023-06-06 21:30:25 -0700

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,142 @@
-# ShareAmeal
+# **SHAREMEAL**
+  
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+---
+
+<br>
+<br>
+
+
+
+![Desktop Img](/client/src/assets/ShareMealDesktop.png) 
+
+
+ 
+
+
+
+
+
+
+
+  
+<br>
+<br>
+<br> 
+
+## *Description*  
+<br>
+<br>
+
+As passionate food lovers, we created SHAREMEAL as an application that allows the user to create and share meal recipes with others.  
+
+We are constantly exploring new flavors, experimenting with ingredients, and creating unique dishes that we would love to share with a  
+
+community of like-minded individuals.  We envision an application that makes recipe creation and sharing effortless.
+
+
+
+<br>
+<br>
+
+## *Technologies*  
+<br>
+
+Technologies used are React, Chakra UI, Bootstrap, Pexels Photocommunity, Raleway Fonts, React-Icons on the front end.  
+
+GraphQL with a Node.js and Express.js server.  
+
+MongoDB and the Mongoose ODM for the database.
+
+JWT was used for authentication.
+
+ 
+
+<br>
+<br>
+
+## *Instructions*  
+<br>  
+
+To view the site, navigate to the URL link to our Heroku deployed application provided below.  
+
+On the navbar select the option to `Login/Signup`.  
+
+When form loads, select `Signup` at the top.  
+
+Input username of choice, an email address and password of 8 characters minimum.  
+
+After authentication, the user gains access to options to create, update, save and delete recipes.  
+
+Other options for the user are categories such as Italian, Asian, European and American.  
+
+
+
+
+
+
+
+
+
+
+<br>
+<br>
+
+
+
+## *GitHub Repository*  
+
+[Link Here](https://github.com/LL8719/ShareMeal)
+<br>
+<br>
+
+## *Deployed Application URL on Heroku*
+
+[Link Here](https://joijsjpkjksjkjksjkjkkj/)
+
+
+### *License*  
+This project is licensed under the [MIT License Copyright (c) 2023 LL8719 ](https://opensource.org/licenses/MIT)
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- client/src/components/UserRecipes/index.js
+++ client/src/components/UserRecipes/index.js
@@ -1,41 +1,36 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_SINGLE_USER } from '../../utils/queries';
-
 const UserRecipes = () => {
-	const { userId } = useParams();
-
-	const { loading, data } = useQuery(QUERY_SINGLE_USER, {
-		variables: { userId },
-	});
-
-	if (loading) {
-		return <div>Loading...</div>;
-	}
-
-	const user = data?.getUserById;
-
-	return (
-		<div className="container">
-			<h3 className="text-primary">User Recipes</h3>
-			{user ? (
-				<div>
-					<h4 className="text-secondary">{user.username}'s Recipes</h4>
-					{user.recipes.map((recipe) => (
-						<div key={recipe._id} className="card mb-3">
-							<div className="card-body">
-								<h5 className="card-title text-primary">{recipe.title}</h5>
-								<p className="card-text">{recipe.content}</p>
-							</div>
-						</div>
-					))}
-				</div>
-			) : (
-				<div className="text-danger">User not found</div>
-			)}
-		</div>
-	);
+    const { userId } = useParams();
+    const { loading, data } = useQuery(QUERY_SINGLE_USER, {
+        variables: { userId },
+    });
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+    const user = data?.getUserById;
+    return (
+        <div className="container">
+            <Link to="/">Back to Home</Link>{' '}
+            {/* Add the Link component to create a button that links to the home page */}
+            {user ? (
+                <div>
+                    <h4 className="text-secondary">{user.username}'s Recipes</h4>
+                    {user.recipes.map((recipe) => (
+                        <div key={recipe._id} className="card mb-3">
+                            <div className="card-body">
+                                <h5 className="card-title text-primary">{recipe.title}</h5>
+                                <p className="card-text">{recipe.content}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div className="text-danger">User not found</div>
+            )}
+        </div>
+    );
 };
-
 export default UserRecipes;
