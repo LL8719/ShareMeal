d2e794c17a55d192a396a452a5ce05e6321a5064
LL8719 <arsgatslr_87@yahoo.com>
2023-06-08 19:27:51 -0700

    fixed log in on userRecipes

--- client/src/components/UserRecipes/index.js
+++ client/src/components/UserRecipes/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_SINGLE_USER } from '../../utils/queries';
+import Login from '../../pages/Login';
+
 const UserRecipes = () => {
-
 	const { userId } = useParams();
-
 	const { loading, data } = useQuery(QUERY_SINGLE_USER, {
 		variables: { userId },
 	});
+
+	const [showLoginCard, setShowLoginCard] = useState(false);
 
 	if (loading) {
 		return <div>Loading...</div>;
@@ -16,33 +18,38 @@ const UserRecipes = () => {
 
 	const user = data?.getUserById;
 
+	if (!user && !showLoginCard) {
+		setShowLoginCard(true);
+	}
+
 	return (
 		<div className="container">
 			<Link to="/">Back to Home</Link>{' '}
 			{/* Add the Link component to create a button that links to the home page */}
 			{user ? (
 				<div>
-					<h4 className="center my-3">{user.username}'s Recipes</h4>
+					<h4 className="text-secondary">{user.username}'s Recipes</h4>
 					{user.recipes.map((recipe) => (
-
 						<div key={recipe._id} className="card mb-3">
 							<div className="card-body">
 								<h5 className="card-title text-primary">{recipe.title}</h5>
 								<p className="card-text">{recipe.content}</p>
 							</div>
 						</div>
-
 					))}
 				</div>
-			) : (
-				<div
-					className="text-danger"
-					style={{ fontSize: '34px', fontWeight: 'bold' }}>
-					You Need to be Logged in!
+			) : showLoginCard ? (
+				<div>
+					<div
+						className="text-danger"
+						style={{ fontSize: '34px', fontWeight: 'bold' }}>
+						You Need to be Logged in to see this User's Recipes!!
+					</div>
+					<Login />
 				</div>
-			)}
+			) : null}
 		</div>
 	);
 };
-export default UserRecipes;
 
+export default UserRecipes;
